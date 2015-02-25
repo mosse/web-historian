0d9c62102bed75a55c9f643f6e4eb0473d4c8631
mosse <mjemacdonald@gmail.com>
2015-02-25 15:40:59 -0800

    Serving static assets

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,11 +10,22 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset, callback) {
+exports.serveAssets = function(response, filename, pathname) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  var filePath = (path.join(__dirname, pathname, filename ));
+  response.writeHead(200, headers);
+  var fileContents = fs.readFile(filePath, function(err, data){
+  response.end(data);
+  })
 };
 
 
 
 // As you progress, keep thinking about what helper functions you can put here!
+exports.redirect = function(response, location) {
+  response.statusCode = 303;
+  response.setHeader("Location", location);
+  response.end();
+};
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,27 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('http-helpers.js');
 // require more modules/folders here!
 
+var actions = {
+  'GET': function(req, res) {
+    httpHelper.serveAssets(res, 'index.html', './public/')
+  },
+  'POST': function(req, res) {
+    // httpHelper.redirect(res, './public/loading.html');
+    console.log('POST');
+  },
+  'OPTIONS': function() {
+
+  }
+};
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action) {
+    action(req, res);
+  } else {
+    res.end(404, 'ERROR');
+  }
 };
